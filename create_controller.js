0c57b08defbdfbe34f56a8d7ee519af47b4d46c4
Chris Golding <cpg_94@outlook.com>
2018-07-09 08:06:53 +0100

    try/catch

--- create_controller.js
+++ create_controller.js
@@ -31,18 +31,19 @@ export default new ${name}Controller();`
 
 
 async function main() {
-	const question = [
-		{
-			message: "Controller Name?",
-			type: "input",
-			name: "answer"
-		},
-		{
-			message: "Endpoint?",
-			type: "input",
-			name: "endpoint"
-		}
-	]
+	try {
+		const question = [
+			{
+				message: "Controller Name?",
+				type: "input",
+				name: "answer"
+			},
+			{
+				message: "Endpoint?",
+				type: "input",
+				name: "endpoint"
+			}
+		]
 	const controllerName = await inquirer.prompt(question);
 
 	if (!controllerName.answer) {
@@ -50,6 +51,9 @@ async function main() {
 		return
 	}
 	createTemplate(controllerName.answer, controllerName.endpoint);
+	} catch (error) {
+		throw error;
+	}
 }
 
 
@@ -64,71 +68,75 @@ async function createTemplate(name, endpoint) {
 }
 
 async function addRoutes(routeName, endpoint) {
-	const app = await readFile('./src/app.js');
-	const { tree, locations, comments } = parseModuleWithLocation(app.toString('utf-8'));
+	try {
+		const app = await readFile('./src/app.js');
+		const { tree, locations, comments } = parseModuleWithLocation(app.toString('utf-8'));
 
-	let exists = false;
-	let lastCtrlIdx = null;
-	let lastRouteIdx = null;
+		let exists = false;
+		let lastCtrlIdx = null;
+		let lastRouteIdx = null;
 
-	tree.items.forEach((item, i) => {
-		if (item.type === 'Import' && item.defaultBinding && item.defaultBinding.name.includes('Controller')) {
-			lastCtrlIdx = i;
-		}
-		if (
-			item.type === 'ExpressionStatement' &&
-			item.expression.arguments &&
-			item.expression.arguments.filter(arg => arg.callee && arg.callee.property === 'routes').length
-		) {
-			lastRouteIdx = i;
-		}
-		if (
-			item.type === 'ExpressionStatement' &&
-			item.expression.arguments &&
-			item.expression.arguments.filter(arg => arg.callee && arg.callee.object && arg.callee.object.name === `${routeName}Controller`).length
-		) {
-			exists = true;
-		}
-	});
+		tree.items.forEach((item, i) => {
+			if (item.type === 'Import' && item.defaultBinding && item.defaultBinding.name.includes('Controller')) {
+				lastCtrlIdx = i;
+			}
+			if (
+				item.type === 'ExpressionStatement' &&
+				item.expression.arguments &&
+				item.expression.arguments.filter(arg => arg.callee && arg.callee.property === 'routes').length
+			) {
+				lastRouteIdx = i;
+			}
+			if (
+				item.type === 'ExpressionStatement' &&
+				item.expression.arguments &&
+				item.expression.arguments.filter(arg => arg.callee && arg.callee.object && arg.callee.object.name === `${routeName}Controller`).length
+			) {
+				exists = true;
+			}
+		});
 
-	if (exists) {
-		console.log(`☠️ Controller already exists`);
-		return;
+		if (exists) {
+			console.log(`☠️ Controller already exists`);
+			return;
+		}
+
+		tree.items.splice(lastCtrlIdx + 1, 0, {
+			type: 'Import',
+			defaultBinding: { type: 'BindingIdentifier', name: `${routeName}Controller` },
+			namedImports: [],
+			moduleSpecifier: `./controllers/${routeName}Controller`
+		});
+
+		tree.items.splice(lastRouteIdx + 2, 0, {
+			type: 'ExpressionStatement',
+			expression: {
+				type: 'CallExpression',
+				callee: {
+					type: 'StaticMemberExpression',
+					object: { type: 'IdentifierExpression', name: 'app' },
+					property: 'use'
+				},
+				arguments: [
+					{ type: 'LiteralStringExpression', value: `/${endpoint.toLowerCase()}` },
+					{
+						type: 'CallExpression',
+						callee: {
+							type: 'StaticMemberExpression',
+							object: { type: 'IdentifierExpression', name: `${routeName}Controller` },
+							property: 'routes'
+						},
+						arguments: []
+					}
+				]
+			}
+		});
+
+		await writeFile(`./src/app.js`, prettier.format(codegen.default(tree)));
+		setDone()
+	} catch (error){
+		throw error
 	}
-
-	tree.items.splice(lastCtrlIdx + 1, 0, {
-		type: 'Import',
-		defaultBinding: { type: 'BindingIdentifier', name: `${routeName}Controller` },
-		namedImports: [],
-		moduleSpecifier: `./controllers/${routeName}Controller`
-	});
-
-	tree.items.splice(lastRouteIdx + 2, 0, {
-		type: 'ExpressionStatement',
-		expression: {
-			type: 'CallExpression',
-			callee: {
-				type: 'StaticMemberExpression',
-				object: { type: 'IdentifierExpression', name: 'app' },
-				property: 'use'
-			},
-			arguments: [
-				{ type: 'LiteralStringExpression', value: `/${endpoint.toLowerCase()}` },
-				{
-					type: 'CallExpression',
-					callee: {
-						type: 'StaticMemberExpression',
-						object: { type: 'IdentifierExpression', name: `${routeName}Controller` },
-						property: 'routes'
-					},
-					arguments: []
-				}
-			]
-		}
-	});
-
-	await writeFile(`./src/app.js`, prettier.format(codegen.default(tree)));
-	setDone()
 }
 
 function setDone() {
